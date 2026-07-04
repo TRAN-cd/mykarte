cd4426683e4451ab4ac16c12a756f6360c809db7
TRAN-cd <ngockiet.tran11@gmail.com>
2026-07-04 16:35:55 +0900

    feat: パスワードリセット機能を実装

---
+++ src/app/(auth)/forget_password/input/password/page.tsx
@@ -0,0 +1,107 @@
+'use client'
+
+import { supabase } from "@/app/_libs/supabase";
+import { SubmitHandler } from "react-hook-form"
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Input } from "@/app/_components/Input"
+import { AuthButton } from "@/app/_components/AuthButton";
+import { Card } from "@/app/_components/Card";
+import { useAuthForm } from "@/app/_hooks/useAuthForm";
+
+type Inputs = {
+  password: string;
+};
+
+export default function Page() {
+  const [visible, setVisible] = useState(false)
+  const router = useRouter()
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: {
+      isDirty,
+      isValid,
+      isSubmitting,
+      errors,
+    },
+  } = useAuthForm<Inputs>({ password: "" })
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    try {
+      const { error } = await supabase.auth.updateUser({
+        password: data.password,
+      })
+
+      if (error) {
+        setError("root.serverError", {
+          type: "manual",
+          message: "パスワードは半角英数字・記号のみ 8字以上で設定してください。"
+        })
+        return
+      }
+
+      router.push('/forget_password/complete/')
+    } catch (error) {
+      setError("root.serverError", {
+        type: "manual",
+        message: "エラーが発生しました。再度お試しください。"
+      })
+    }
+  }
+
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center w-full">
+      {isSubmitting && <p className="text-(--color-primary) text-xs border border-(--color-sub) bg-(--color-bg) max-w-115 w-full text-center p-3 rounded-[10px]">送信中</p>}
+      {errors.root?.serverError && (
+        <p className="text-(--color-danger) text-xs border border-(--color-danger) bg-(--color-danger-bg) max-w-115 w-full text-center p-3 rounded-[10px]">
+          {errors.root.serverError.message}
+        </p>
+      )}
+
+      <Card>
+        <h2 className="text-center text-2xl font-bold">パスワードの再設定</h2>
+        <p className="text-center text-[15px] leading-relaxed">新しいパスワードを入力し、<br />【設定する】を押してください。</p>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3">
+          <Input
+            htmlFor="password"
+            title="パスワード(半角英数字・記号のみ 8字以上)"
+            type={visible ? 'text' : 'password'}
+            id="password"
+            placeholder="••••••••"
+            registerProps={register("password", {
+              required: "パスワードが入力されていません。",
+              pattern: {
+                value: /^[a-zA-Z0-9!-/:-@[-`{-~]{8,}$/,
+                message: "半角英数字・記号のみ 8字以上で入力してください。"
+              }
+            })}
+          />
+          {errors.password &&
+            <span className="text-(--color-danger) text-xs">{errors.password.message}</span>
+          }
+
+          <div className="flex justify-end items-center gap-2">
+            <input
+              type="checkbox"
+              name="checkbox"
+              id="checkbox"
+              checked={visible}
+              onChange={(e) => setVisible(e.target.checked)}
+              className="border-(--color-primary)"
+            />
+            <label htmlFor="checkbox" className="text-[10px]">パスワードを表示</label>
+          </div>
+
+          <AuthButton
+            text="設定する"
+            disabled={!isDirty || !isValid || isSubmitting}
+          />
+        </form>
+      </Card>
+    </div>
+  )
+}
---
+++ src/app/(auth)/forget_password/page.tsx
@@ -0,0 +1,107 @@
+'use client'
+
+import { supabase } from "@/app/_libs/supabase";
+import { SubmitHandler } from "react-hook-form"
+import { useState } from "react";
+import { Input } from "@/app/_components/Input"
+import { AuthButton } from "@/app/_components/AuthButton";
+import { Card } from "@/app/_components/Card";
+import { useAuthForm } from "@/app/_hooks/useAuthForm";
+
+const appUrl = process.env.NEXT_PUBLIC_APP_URL;
+
+
+type Inputs = {
+  email: string;
+};
+
+export default function Page() {
+  const [isSuccess, setIsSuccess] = useState(false)
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: {
+      isDirty,
+      isValid,
+      isSubmitting,
+      errors,
+    },
+  } = useAuthForm<Inputs>({ email: "" })
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    try {
+      const { error } = await supabase.auth.resetPasswordForEmail(
+        data.email,
+        {
+          redirectTo: `${appUrl}/forget_password/input/password`,
+        })
+
+      if (error) {
+        console.log(error)
+        setError("root.serverError", {
+          type: "manual",
+          message: "しばらく時間をおいてから、再度お試しください。"
+        })
+        return
+      }
+
+      setIsSuccess(true)
+    } catch (error) {
+      setError("root.serverError", {
+        type: "manual",
+        message: "エラーが発生しました。もう一度お試しください。"
+      })
+    }
+  }
+
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center w-full">
+      {isSuccess && (
+        <p className="text-(--color-text) text-xs border border-(--color-sub) bg-(--color-bg) max-w-115 w-full text-center p-3 rounded-[10px]">
+        パスワードリセットに必要なメールを送信しました。<br />
+        30分以内にメール記載のリンクをクリックし、<br />
+        パスワードの再設定に進んでください。</p>
+      )}
+      {isSubmitting && <p className="text-(--color-primary) text-xs border border-(--color-sub) bg-(--color-bg) max-w-115 w-full text-center p-3 rounded-[10px]">送信中</p>}
+      {errors.root?.serverError && (
+        <p className="text-(--color-danger) text-xs border border-(--color-danger) bg-(--color-danger-bg) max-w-115 w-full text-center p-3 rounded-[10px]">
+          {errors.root.serverError.message}
+        </p>
+      )}
+
+      <Card>
+        <h2 className="text-center text-2xl font-bold">パスワードをリセットする</h2>
+        <p className="text-center text-[15px] leading-relaxed">
+          「パスワード再設定ページのURL」を、<br />登録メールアドレスに送信します。<br />登録メールアドレスを入力し【送信する】を<br />押してください。
+        </p>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3">
+          <Input
+            htmlFor="email"
+            title="メールアドレス"
+            type="email"
+            id="email"
+            placeholder="example@mykarte.com"
+            registerProps={register("email", {
+              required: "メールアドレスが入力されていません。",
+              pattern: {
+                value: /[\w\.-]+@[\w\.-]+\.\w{2,4}/,
+                message: "正しいメールアドレス形式で入力してください。"
+              }
+            })}
+          />
+          {errors.email &&
+            <span className="text-(--color-danger) text-xs">{errors.email.message}</span>
+          }
+
+          <AuthButton
+            text="送信する"
+            disabled={!isDirty || !isValid || isSubmitting}
+          />
+        </form>
+      </Card>
+    </div>
+  )
+}
