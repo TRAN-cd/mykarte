3cd643760059e49a7882cf2c6accb2eb4ab9f654
TRAN-cd <ngockiet.tran11@gmail.com>
2026-07-07 23:04:20 +0900

    refactor: コンポーネント化

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -8,12 +8,10 @@ import { Input } from "@/app/_components/Input"
 import { AuthButton } from "@/app/_components/AuthButton";
 import { DividerLine } from "@/app/_components/DividerLine";
 import Link from "next/link";
-import Image from "next/image";
 import { Card } from "@/app/_components/Card";
 import { useAuthForm } from "@/app/_hooks/useAuthForm";
 import { getAuthErrorMessage } from "@/app/_libs/getAuthErrorMessage";
-
-const appUrl = process.env.NEXT_PUBLIC_APP_URL;
+import { GoogleAuthButton } from "@/app/_components/GoogleAuthButton";
 
 type Inputs = {
   email: string;
@@ -61,15 +59,6 @@ export default function Page() {
         message: getAuthErrorMessage(undefined)
       })
     }
-  }
-
-  const handleGoogleLogin = async () => {
-    await supabase.auth.signInWithOAuth({
-      provider: 'google',
-      options: {
-        redirectTo: `${appUrl}/mykarte`
-      },
-    })
   }
 
   return (
@@ -145,16 +134,7 @@ export default function Page() {
 
         <DividerLine text="他の方法でログインする" />
 
-        <div className="flex items-center gap-6">
-          <button
-            onClick={handleGoogleLogin}
-            className="border border-(--color-sub) px-17.25 py-1.75 rounded-[30px] hover:opacity-70 duration-300 cursor-pointer">
-            <Image src="/icons/google.png" alt="googleアカウント" width={30} height={26} />
-          </button>
-          <div className="border border-(--color-sub) px-17.25 py-1.75 rounded-[30px] hover:opacity-70 duration-300">
-            <Image src="/icons/apple.png" alt="Appleアカウント" width={30} height={26} />
-          </div>
-        </div>
+        <GoogleAuthButton label="Googleでログイン"/>
 
         <Link href="/signup" className="text-center text-(--color-primary) text-xs hover:opacity-70 duration-300">
           myカルテ の新規登録はこちら
