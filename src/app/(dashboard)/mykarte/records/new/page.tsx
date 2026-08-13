da0cf85062579c0fdeee6c94a33be573754ecdcd
TRAN-cd <ngockiet.tran11@gmail.com>
2026-08-13 23:44:53 +0900

    wip: 記録日入力欄の実装を検討中

--- src/app/(dashboard)/mykarte/records/new/page.tsx
+++ src/app/(dashboard)/mykarte/records/new/page.tsx
@@ -1,13 +1,43 @@
 'use client'
 
+import { useForm } from "react-hook-form";
+import { useRef } from "react";
+import { PageHeader } from "@/app/_components/PageHeader";
+import { RecordItemTitle } from "@/app/_components/RecordItemTitle";
+import { CalendarIcon } from "@/app/_components/icons/CalenderIcon"
+
+interface RecordFormInputs {
+  recordDate: Date
+  // recordType: enum
+}
+
 export default function NewRecords() {
+  const { register } = useForm<RecordFormInputs>()
+  const { ref, ...rest } = register("recordDate")
+  const dateInputRef = useRef<HTMLInputElement>(null)
+
   return (
-    <>
-      <main className="flex-1 flex">
-        <div className="font-bold text-xl w-full flex justify-center items-center h-[80vh]">
-          モーダルではない新規記録ページ
-        </div>
+    <div className="px-6 py-5">
+      <PageHeader pageTitle="新規記録" />
+
+      <main className="flex flex-col gap-6 p-5 bg-white rounded-[20px] border-(--color-bg) border">
+          <form action="" className="flex flex-col gap-3">
+            <RecordItemTitle itemTitle="記録日"/>
+            <input 
+              id="recordDate"
+              type="date"
+              ref={ref}
+              className="opacity-0 absolute"
+            />
+            <div 
+              onClick={() => ref.current?.showPicker()} 
+              className="bg-(--color-card-bg) p-3 rounded-[10px] border border-(--color-primary) font-en text-xs w-40"
+            >
+              <p></p>
+              <CalendarIcon className="text-(--color-sub) w-3.5 h-3.5" />
+            </div>
+          </form>
       </main>
-    </>
+    </div>
   );
 }
---
+++ src/app/_components/RecordItemTitle.tsx
@@ -0,0 +1,16 @@
+type Props = {
+  itemTitle: string
+}
+
+export const RecordItemTitle = ({itemTitle}: Props) => {
+  return (
+    <div className="pb-2.5 border-b border-b-(--color-sub)/20">
+      <label 
+        htmlFor="recordDate"
+        className="flex items-center text-[13px] font-medium pl-2 border-l-2 border-(--color-primary)"
+      >
+          {itemTitle}
+      </label>
+    </div>
+  )
+}
