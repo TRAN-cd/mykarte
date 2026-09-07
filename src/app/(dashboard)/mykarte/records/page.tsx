330505b77e975fa3f8044779b3f576552195b117
TRAN-cd <ngockiet.tran11@gmail.com>
2026-09-07 23:12:22 +0900

    feat: 記録一覧に実データ（日付・カテゴリー等）を表示

--- src/app/(dashboard)/mykarte/records/page.tsx
+++ src/app/(dashboard)/mykarte/records/page.tsx
@@ -9,10 +9,43 @@ import { SevereIcon } from "@/app/_components/icons/SevereIcon";
 import { ArrowIcon } from "@/app/_components/icons/ArrowIcon";
 import Link from "next/link";
 import { DeleteIcon } from "@/app/_components/icons/DeleteIcon";
+import { RecordResponse } from "@/app/api/records/route";
+import { RecordType } from "@/generated/prisma/enums";
+
+const formatDate = (dateString: string | Date) => {
+  const date = new Date(dateString);
+
+  return new Intl.DateTimeFormat('ja-JP', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    weekday: 'short'
+  }).format(date);
+};
+
+const convertRecordType = (type: RecordType) => {
+  switch (type) {
+    case "DAILY":
+      return "日常の記録";
+    case "MEDICAL":
+      return "診療の内容";
+    default:
+      throw new Error("不正なrecordTypeの値です。");
+  }
+}
+
+// const convertSeverityLevel = (level: number | undefined | null) => {
+
+// }
 
 export default function NewRecords() {
-  const { data } = useFetch<GetCategoryResponse>("/api/categories/")
-  const categories = data?.categories || []
+  const { data: categoriesData } = useFetch<GetCategoryResponse>("/api/categories/")
+  const categories = categoriesData?.categories || []
+  // const category = categories.find((cat) => cat.id === Element.reco)
+
+  const { data: recordsData, error, isLoading } = useFetch<RecordResponse>("/api/records");
+  const records = recordsData?.records || []
+  console.log(records);
 
   const handleDeleteConfirm = () => {
     const isConfirmed = confirm("削除しますか？")
@@ -23,6 +56,20 @@ export default function NewRecords() {
       return true
     }
   }
+
+  if (isLoading) return (
+    <div className="px-6 py-5">
+      <p className="text-sm text-(--color-sub) text-center py-10">読み込み中...</p>
+    </div>
+  );
+  
+  if (error) return (
+    <div className="px-6 py-5">
+      <p className="text-sm text-(--color-danger) text-center py-10 bg-(--color-danger-bg) rounded-[5px] border border-(--color-danger)">
+        記録の取得に失敗しました。
+      </p>
+    </div>
+  );
 
   return (
     <div className="px-6 py-5 w-full">
@@ -79,56 +126,66 @@ export default function NewRecords() {
       </div>
 
       <ul className="flex flex-col gap-3">
-        <li className="relative bg-white p-5 rounded-[10px] border border-transparent shadow-none duration-300 hover:border-(--color-primary) hover:shadow-[0px_10px_50px_0px_rgba(28,43,36,0.1)]">
-          <div className="flex items-center gap-4 pb-4">
-            <p className="font-en text-sm font-medium ">2026月5月30日(土)</p>
-            <div className="flex items-center gap-1.5">
-              <RecordIcon className="transition-colors w-3.5 text-(--color-primary) -mb-0.75" />
-              <p className="text-xs font-medium">日常の記録</p>
-            </div>
-          </div>
-          <div className="flex">
-            <div className="flex flex-col gap-2.5 max-w-22.5 w-full py-2.5 pr-2.5 border-r border-(--color-sub)/20">
-              <p className="w-fit bg-(--color-bg) px-2 py-0.5 rounded-xl border border-(--color-primary) text-[10px] font-medium text-(--color-primary)">皮膚科</p>
-              <div className="w-fit flex items-center gap-1 text-[10px] py-0.5 px-1.5 border rounded-xl text-(--color-danger) font-medium border-(--color-danger) bg-(--color-danger-bg)">
-                <SevereIcon className="w-3" />
-                <span className="">重度</span>
-              </div>
-              <ul className="flex flex-wrap gap-1.5">
-                <li className="inline-block bg-(--color-bg) px-2 py-0.5 rounded-xl border border-(--color-primary) text-[10px] font-medium text-(--color-primary)">昼</li>
-                <li className="inline-block bg-(--color-bg) px-2 py-0.5 rounded-xl border border-(--color-primary) text-[10px] font-medium text-(--color-primary)">夕</li>
-              </ul>
-            </div>
-            <div className="flex flex-col gap-3 py-2.5 pl-2.5 pr-10">
-              <p className="text-sm font-medium">ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダ</p>
-              <div className="w-fit flex flex-col gap-1.5 bg-(--color-card-bg) px-4 py-2.5 rounded-[10px]">
-                <dl className="flex gap-1.5 text-[10px] font-medium">
-                  <dt className="text-(--color-sub) whitespace-nowrap">対処</dt>
-                  <dd>もらった軟膏を塗った。</dd>
-                </dl>
-                <dl className="flex gap-1.5 text-[10px] font-medium">
-                  <dt className="text-(--color-sub)">次回受診日</dt>
-                  <dd className="font-en">2026年6月20日</dd>
-                </dl>
-              </div>
-            </div>
-          </div>
+        {records.map((elem) => {
+          const category = categories.find((cat) => cat.id === elem.recordCategories[0].categoryId);
 
-          <div className="absolute right-5 bottom-5">
-            <div className="flex gap-2">
-              <button onClick={handleDeleteConfirm} className="block w-7 h-7 bg-white rounded-[50%] border border-(--color-text)/20 duration-300 hover:border-(--color-danger) hover:bg-(--color-danger-bg) group cursor-pointer">
-                <div className="h-full flex justify-center items-center">
-                  <DeleteIcon className="text-(--color-text) w-3 h-3 duration-300 group-hover:text-(--color-danger)" />
+          return(
+            <li key={elem.id} className="relative bg-white p-5 rounded-[10px] border border-transparent shadow-none duration-300 hover:border-(--color-primary) hover:shadow-[0px_10px_50px_0px_rgba(28,43,36,0.1)]">
+              <div className="flex items-center gap-4 pb-4">
+                <p className="font-en text-sm font-medium">{formatDate(elem.recordAt)}</p>
+                <div className="flex items-center gap-1.5">
+                  <RecordIcon className="transition-colors w-3.5 text-(--color-primary) -mb-0.75" />
+                  <p className="text-xs font-medium">{convertRecordType(elem.recordType)}</p>
                 </div>
-              </button>
-              <Link href="/mykarte/records/1" className="block w-7 h-7 bg-(--color-primary) border border-transparent rounded-[50%] duration-300 hover:bg-white hover:border-(--color-primary) group">
-                <div className="h-full flex justify-center items-center">
-                  <ArrowIcon className="text-white w-2 h-2.75 -mr-0.5 duration-300 group-hover:text-(--color-primary)" />
+              </div>
+              <div className="flex">
+                <div className="flex flex-col gap-2.5 max-w-22.5 w-full py-2.5 pr-2.5 border-r border-(--color-sub)/20">
+                  <p className="w-fit bg-(--color-bg) px-2 py-0.5 rounded-xl border border-(--color-primary) text-[10px] font-medium text-(--color-primary)">
+                    {category?.name ?? "選択なし"}
+                  </p>
+                  <div className="w-fit flex items-center gap-1 text-[10px] py-0.5 px-1.5 border rounded-xl text-(--color-danger) font-medium border-(--color-danger) bg-(--color-danger-bg)">
+                    <SevereIcon className="w-3" />
+                    <span className="">重度</span>
+                  </div>
+                  <ul className="flex flex-wrap gap-1.5">
+                    <li className="inline-block bg-(--color-bg) px-2 py-0.5 rounded-xl border border-(--color-primary) text-[10px] font-medium text-(--color-primary)">昼</li>
+                    <li className="inline-block bg-(--color-bg) px-2 py-0.5 rounded-xl border border-(--color-primary) text-[10px] font-medium text-(--color-primary)">夕</li>
+                  </ul>
                 </div>
-              </Link>
-            </div>
-          </div>
-        </li>
+                <div className="flex flex-col gap-3 py-2.5 pl-2.5 pr-10">
+                  <p className="text-sm font-medium">{elem.content}</p>
+                  <div className="w-fit flex flex-col gap-1.5 bg-(--color-card-bg) px-4 py-2.5 rounded-[10px]">
+                    <dl className="flex gap-1.5 text-[10px] font-medium">
+                      <dt className="text-(--color-sub) whitespace-nowrap">対処</dt>
+                      <dd>{elem.treatment ? elem.treatment : "-"}</dd>
+                    </dl>
+                    <dl className="flex gap-1.5 text-[10px] font-medium">
+                      <dt className="text-(--color-sub)">次回受診日</dt>
+                      <dd className="font-en">
+                        {elem.nextVisit ? formatDate(elem.nextVisit) : "-"}
+                      </dd>
+                    </dl>
+                  </div>
+                </div>
+              </div>
+
+              <div className="absolute right-5 bottom-5">
+                <div className="flex gap-2">
+                  <button onClick={handleDeleteConfirm} className="block w-7 h-7 bg-white rounded-[50%] border border-(--color-text)/20 duration-300 hover:border-(--color-danger) hover:bg-(--color-danger-bg) group cursor-pointer">
+                    <div className="h-full flex justify-center items-center">
+                      <DeleteIcon className="text-(--color-text) w-3 h-3 duration-300 group-hover:text-(--color-danger)" />
+                    </div>
+                  </button>
+                  <Link href="/mykarte/records/1" className="block w-7 h-7 bg-(--color-primary) border border-transparent rounded-[50%] duration-300 hover:bg-white hover:border-(--color-primary) group">
+                    <div className="h-full flex justify-center items-center">
+                      <ArrowIcon className="text-white w-2 h-2.75 -mr-0.5 duration-300 group-hover:text-(--color-primary)" />
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </li>
+          )
+        })}
 
       </ul>
     </div>
