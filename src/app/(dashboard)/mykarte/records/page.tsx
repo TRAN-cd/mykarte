6522bc351025be476a08ecfbcf71fd161f357faf
TRAN-cd <ngockiet.tran11@gmail.com>
2026-09-23 16:31:44 +0900

    feat: 記録一覧にフィルター機能を実装

--- src/app/(dashboard)/mykarte/records/page.tsx
+++ src/app/(dashboard)/mykarte/records/page.tsx
@@ -16,7 +16,7 @@ import { ModerateIcon } from "@/app/_components/icons/ModerateIcon";
 import { HospitalIcon } from "@/app/_components/icons/HospitalIcon";
 import { handleApiError } from "@/app/_libs/handleApiError";
 import { apiFetch } from "@/app/_libs/apiFetch";
-import { useState } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
 
 const formatDate = (dateString: string | Date) => {
   const date = new Date(dateString);
@@ -32,9 +32,9 @@ const formatDate = (dateString: string | Date) => {
 const displayRecordType = (type: RecordType) => {
   switch (type) {
     case "DAILY":
-      return {icon: RecordIcon, label: "日常の記録"};
+      return { icon: RecordIcon, label: "日常の記録" };
     case "MEDICAL":
-      return {icon: HospitalIcon, label: "診療の内容"};
+      return { icon: HospitalIcon, label: "診療の内容" };
     default:
       throw new Error("不正なrecordTypeの値です。");
   }
@@ -95,27 +95,40 @@ const convertTimeZone = (tz: TimeZone) => {
 }
 
 export default function NewRecords() {
+  // カテゴリー取得
   const { data: categoriesData } = useFetch<GetCategoryResponse>("/api/categories/")
   const categories = categoriesData?.categories || []
-  const [paramCategory, setParamCategory] = useState("allCategories")
-  const [paramRecordType, setParamRecordType] = useState("allRecordType")
-  const [paramPeriod, setParamPeriod] = useState("allPeriod")
 
-  const params = new URLSearchParams()
-  if(paramCategory !== "allCategories") {
-    params.set("category", paramCategory)
-  }
-  if(paramRecordType !== "allRecordType") {
-    params.set("type", paramRecordType)
-  }
-  if(paramPeriod !== "allPeriod") {
-    params.set("period", paramPeriod)
-  }
-  const urlParams = params.toString()
+  // URL・フィルター
+  const router = useRouter();
+  //現在のURLのクエリパラメータ(?category=22のような部分)を、読み取り専用の形で取得
+  const searchParams = useSearchParams()
+  //searchParamsから、category, type, periodの値をそれぞれ取り出す
+  const paramCategory = searchParams.get("category") ?? "allCategories"
+  const paramRecordType = searchParams.get("type") ?? "allRecordType"
+  const paramPeriod = searchParams.get("period") ?? "allPeriod"
 
-  const { data: recordsData, error, isLoading, mutate } = useFetch<RecordResponse>(`/api/records${ urlParams ? "?"+urlParams : ""}`);
+  type ParamName = "category" | "type" | "period"
+
+  const updateParam = (name: ParamName, value: string) => {
+    // 現在のsearchParamsの内容をコピーして、編集可能な新しいオブジェクトnewParamsを作成
+    const newParams = new URLSearchParams(searchParams)
+    if (value.startsWith("all")) {
+      newParams.delete(name)
+    } else {
+      newParams.set(name, value)
+    }
+    const newParamsString = newParams.toString()
+    router.push(`/mykarte/records${newParamsString ? "?" + newParamsString : ""}`)
+  }
+
+  const urlParams = searchParams.toString()
+
+  // 記録データ取得
+  const { data: recordsData, error, isLoading, mutate } = useFetch<RecordResponse>(`/api/records${urlParams ? "?" + urlParams : ""}`);
   const records = recordsData?.records || []
 
+  // 削除処理
   const handleDelete = async (id: number) => {
     try {
       const isConfirmed = confirm("削除しますか？")
@@ -154,10 +167,9 @@ export default function NewRecords() {
           <Image src="/images/shared/icon_arrow02.svg" alt="" width="16" height="16" className="absolute top-1/2 -translate-y-1/2 right-3 pointer-events-none" />
           <label htmlFor="category-select"></label>
           <select
-            name="categories"
             id="category-select"
             value={paramCategory}
-            onChange={(e) => {setParamCategory(e.target.value)}}
+            onChange={(e) => updateParam("category", e.target.value)}
             className="w-full bg-(--color-card-bg) border border-(--color-sub) rounded-[10px] px-3 py-2 text-[10px] appearance-none"
           >
             <option value="allCategories">全てのカテゴリー</option>
@@ -171,10 +183,9 @@ export default function NewRecords() {
           <Image src="/images/shared/icon_arrow02.svg" alt="" width="16" height="16" className="absolute top-1/2 -translate-y-1/2 right-3 pointer-events-none" />
           <label htmlFor="recordType-select"></label>
           <select
-            name="recordType"
             id="recordType-select"
             value={paramRecordType}
-            onChange={(e) => {setParamRecordType(e.target.value)}}
+            onChange={(e) => updateParam("type", e.target.value)}
             className="w-full bg-(--color-card-bg) border border-(--color-sub) rounded-[10px] px-3 py-2 text-[10px] appearance-none"
           >
             <option value="allRecordType">すべての記録タイプ</option>
@@ -187,10 +198,9 @@ export default function NewRecords() {
           <Image src="/images/shared/icon_arrow02.svg" alt="" width="16" height="16" className="absolute top-1/2 -translate-y-1/2 right-3 pointer-events-none" />
           <label htmlFor="period-select"></label>
           <select
-            name="period"
             id="period-select"
             value={paramPeriod}
-            onChange={(e) => {setParamPeriod(e.target.value)}}
+            onChange={(e) => updateParam("period", e.target.value)}
             className="w-full bg-(--color-card-bg) border border-(--color-sub) rounded-[10px] px-3 py-2 text-[10px] appearance-none"
           >
             <option value="allPeriod">全期間</option>
@@ -205,81 +215,90 @@ export default function NewRecords() {
       </div>
 
       <ul className="flex flex-col gap-3">
-        {records.map((elem) => {
-          const category = categories.find((cat) => cat.id === elem.recordCategories[0].categoryId);
-          const recordTypeInfo = displayRecordType(elem.recordType)
-          const RecordTypeIcon = recordTypeInfo.icon
-          const severityInfo = displaySeverityLevel(elem.severityLevel)
-          const SeverityIcon = severityInfo.icon
+        {records.length === 0 ? (
+          <div className="flex flex-col items-center">
+            <p className="text-sm text-(--color-sub) text-center py-10">
+              該当する記録がありません。
+            </p>
+            <Image src="/images/avatar/thinking.png" alt="" width="100" height="100" />
+          </div>
 
-          return (
-            <li key={elem.id} className="relative bg-white p-5 rounded-[10px] border border-transparent shadow-none duration-300 hover:border-(--color-primary) hover:shadow-[0px_10px_50px_0px_rgba(28,43,36,0.1)]">
-              <div className="flex items-center gap-4 pb-4">
-                <p className="font-en text-sm font-medium">{formatDate(elem.recordAt)}</p>
-                <div className="flex items-center gap-1.5">
-                  <RecordTypeIcon className="transition-colors w-3.5 text-(--color-primary) -mb-0.75" />
-                  <p className="text-xs font-medium">{recordTypeInfo.label}</p>
-                </div>
-              </div>
-              <div className="flex">
-                <div className="flex flex-col gap-2.5 max-w-22.5 w-full py-2.5 pr-2.5 border-r border-(--color-sub)/20">
-                  <p className="w-fit bg-(--color-bg) px-2 py-0.5 rounded-xl border border-(--color-primary) text-[10px] font-medium text-(--color-primary)">
-                    {category?.name ?? "選択なし"}
-                  </p>
-                  {elem.severityLevel !== null &&
-                    <div className={severityInfo.className}>
-                      {SeverityIcon && <SeverityIcon className="w-3" />}
-                      <span>{severityInfo.label}</span>
-                    </div>
-                  }
-                  {
-                    elem.recordTimeZones.length > 0 &&
-                    <ul className="flex flex-wrap gap-1.5">
-                      {
-                        elem.recordTimeZones.map((tz) => (
-                          <li key={tz.id} className="inline-block bg-(--color-bg) px-2 py-0.5 rounded-xl border border-(--color-primary) text-[10px] font-medium text-(--color-primary)">{convertTimeZone(tz.timeZone)}</li>
-                        ))
-                      }
-                    </ul>
-                  }
-                </div>
-                <div className="flex flex-col gap-3 py-2.5 pl-2.5 pr-10">
-                  <p className="text-sm font-medium">{elem.content}</p>
-                  <div className="w-fit flex flex-col gap-1.5 bg-(--color-card-bg) px-4 py-2.5 rounded-[10px]">
-                    <dl className="flex gap-1.5 text-[10px] font-medium">
-                      <dt className="text-(--color-sub) whitespace-nowrap">対処</dt>
-                      <dd>{elem.treatment ? elem.treatment : "-"}</dd>
-                    </dl>
-                    <dl className="flex gap-1.5 text-[10px] font-medium">
-                      <dt className="text-(--color-sub)">次回受診日</dt>
-                      <dd className="font-en">
-                        {elem.nextVisit ? formatDate(elem.nextVisit) : "-"}
-                      </dd>
-                    </dl>
+        ) : (
+          records.map((elem) => {
+            const category = categories.find((cat) => cat.id === elem.recordCategories[0].categoryId);
+            const recordTypeInfo = displayRecordType(elem.recordType)
+            const RecordTypeIcon = recordTypeInfo.icon
+            const severityInfo = displaySeverityLevel(elem.severityLevel)
+            const SeverityIcon = severityInfo.icon
+
+            return (
+              <li key={elem.id} className="relative bg-white p-5 rounded-[10px] border border-transparent shadow-none duration-300 hover:border-(--color-primary) hover:shadow-[0px_10px_50px_0px_rgba(28,43,36,0.1)]">
+                <div className="flex items-center gap-4 pb-4">
+                  <p className="font-en text-sm font-medium">{formatDate(elem.recordAt)}</p>
+                  <div className="flex items-center gap-1.5">
+                    <RecordTypeIcon className="transition-colors w-3.5 text-(--color-primary) -mb-0.75" />
+                    <p className="text-xs font-medium">{recordTypeInfo.label}</p>
                   </div>
                 </div>
-              </div>
-
-              <div className="absolute right-5 bottom-5">
-                <div className="flex gap-2">
-                  <button 
-                    onClick={() => handleDelete(elem.id)}
-                    className="block w-7 h-7 bg-white rounded-[50%] border border-(--color-text)/20 duration-300 hover:border-(--color-danger) hover:bg-(--color-danger-bg) group cursor-pointer">
-                    <div className="h-full flex justify-center items-center">
-                      <DeleteIcon className="text-(--color-text) w-3 h-3 duration-300 group-hover:text-(--color-danger)" />
+                <div className="flex">
+                  <div className="flex flex-col gap-2.5 max-w-22.5 w-full py-2.5 pr-2.5 border-r border-(--color-sub)/20">
+                    <p className="w-fit bg-(--color-bg) px-2 py-0.5 rounded-xl border border-(--color-primary) text-[10px] font-medium text-(--color-primary)">
+                      {category?.name ?? "選択なし"}
+                    </p>
+                    {elem.severityLevel !== null &&
+                      <div className={severityInfo.className}>
+                        {SeverityIcon && <SeverityIcon className="w-3" />}
+                        <span>{severityInfo.label}</span>
+                      </div>
+                    }
+                    {
+                      elem.recordTimeZones.length > 0 &&
+                      <ul className="flex flex-wrap gap-1.5">
+                        {
+                          elem.recordTimeZones.map((tz) => (
+                            <li key={tz.id} className="inline-block bg-(--color-bg) px-2 py-0.5 rounded-xl border border-(--color-primary) text-[10px] font-medium text-(--color-primary)">{convertTimeZone(tz.timeZone)}</li>
+                          ))
+                        }
+                      </ul>
+                    }
+                  </div>
+                  <div className="flex flex-col gap-3 py-2.5 pl-2.5 pr-10">
+                    <p className="text-sm font-medium">{elem.content}</p>
+                    <div className="w-fit flex flex-col gap-1.5 bg-(--color-card-bg) px-4 py-2.5 rounded-[10px]">
+                      <dl className="flex gap-1.5 text-[10px] font-medium">
+                        <dt className="text-(--color-sub) whitespace-nowrap">対処</dt>
+                        <dd>{elem.treatment ? elem.treatment : "-"}</dd>
+                      </dl>
+                      <dl className="flex gap-1.5 text-[10px] font-medium">
+                        <dt className="text-(--color-sub)">次回受診日</dt>
+                        <dd className="font-en">
+                          {elem.nextVisit ? formatDate(elem.nextVisit) : "-"}
+                        </dd>
+                      </dl>
                     </div>
-                  </button>
-                  <Link href={`/mykarte/records/${elem.id}`} className="block w-7 h-7 bg-(--color-primary) border border-transparent rounded-[50%] duration-300 hover:bg-white hover:border-(--color-primary) group">
-                    <div className="h-full flex justify-center items-center">
-                      <ArrowIcon className="text-white w-2 h-2.75 -mr-0.5 duration-300 group-hover:text-(--color-primary)" />
-                    </div>
-                  </Link>
+                  </div>
                 </div>
-              </div>
-            </li>
-          )
-        })}
 
+                <div className="absolute right-5 bottom-5">
+                  <div className="flex gap-2">
+                    <button
+                      onClick={() => handleDelete(elem.id)}
+                      className="block w-7 h-7 bg-white rounded-[50%] border border-(--color-text)/20 duration-300 hover:border-(--color-danger) hover:bg-(--color-danger-bg) group cursor-pointer">
+                      <div className="h-full flex justify-center items-center">
+                        <DeleteIcon className="text-(--color-text) w-3 h-3 duration-300 group-hover:text-(--color-danger)" />
+                      </div>
+                    </button>
+                    <Link href={`/mykarte/records/${elem.id}`} className="block w-7 h-7 bg-(--color-primary) border border-transparent rounded-[50%] duration-300 hover:bg-white hover:border-(--color-primary) group">
+                      <div className="h-full flex justify-center items-center">
+                        <ArrowIcon className="text-white w-2 h-2.75 -mr-0.5 duration-300 group-hover:text-(--color-primary)" />
+                      </div>
+                    </Link>
+                  </div>
+                </div>
+              </li>
+            )
+          })
+        )}
       </ul>
     </div>
   )
